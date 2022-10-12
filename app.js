c16a0f00e1334c4ea96f556c871a6792b7786f75
Nadezhda Terenteva <nadya_09@inbox.ru>
2022-10-12 15:45:23 +0300

    mongo db update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   // useCreateIndex: true,
   // useFindAndModify: false
